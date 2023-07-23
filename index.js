9c3137fc563729981d7cc9e4ed49325c4fcf9fc2
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-07-23 18:02:21 +0200

    Keep javascript in separate file

---
+++ index.js
@@ -0,0 +1,64 @@
+"use strict";
+
+const fadderukaStartDate = new Date("Aug 14, 2023 10:00:00");
+const interval = setInterval(function () {
+  var distance = fadderukaStartDate.getTime() - new Date().getTime();
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("countdownP").innerHTML =
+    days + "d " + hours + "t " + minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+    clearInterval(setInterval);
+    console.warn("Fadderuka has started");
+  }
+}, 1000);
+const defaultduedate = fadderukaStartDate;
+const newBucketListItem = (newName) =>
+  localStorage.setItem(
+    "db",
+    JSON.stringify(
+      JSON.parse(localStorage.getItem("db")).concat([
+        {
+          name: newName,
+          done: false,
+          due: defaultduedate,
+          doneDate: null,
+          startedDate: new Date(),
+        },
+      ])
+    )
+  ) || generateLists();
+
+const generateLists = () => {
+  document.getElementById("doneUl").innerHTML = "";
+  document.getElementById("todoUl").innerHTML = "";
+  const parsedDb = JSON.parse(localStorage.getItem("db"));
+  parsedDb.forEach((item) => {
+    document.getElementById(
+      item.done ? "doneUl" : "todoUl"
+    ).innerHTML += `<li><input type="checkbox" id="${item.name}" name="${
+      item.name
+    }" class="checkboxes" ${item.done ? "checked" : ""}>${
+      item.name
+    }<label for="${item.name}>${item.name}</label></li>`;
+  });
+  const allCheckboxes = document.getElementsByClassName("checkboxes");
+  for (let i = 0; i < allCheckboxes.length; i++) {
+    allCheckboxes[i].addEventListener("change", (event) => {
+      parsedDb.forEach((item) => {
+        if (item.name === event.target.name) {
+          if (item.done) {
+            item.doneDate = new Date();
+          }
+          item.done = !item.done;
+          generateLists();
+          localStorage.setItem("db", JSON.stringify(parsedDb));
+        }
+      });
+    });
+  }
+};
+
+generateLists();
